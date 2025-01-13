7fa476091a4208facc0780b37f4daefe8dd8457f
CAT <cat5nyago@proton.me>
2025-01-14 05:58:03 +0900

    メインページの修正

---
+++ scripts.js
@@ -0,0 +1,10 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-HPGYFRBNM2');
+
+document.addEventListener("DOMContentLoaded", function() {
+    var counterScript = document.createElement('script');
+    counterScript.src = "//counter1.fc2.com/counter.php?id=89500822";
+    document.body.appendChild(counterScript);
+});
